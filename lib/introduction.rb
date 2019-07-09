bb2e368e4c440826ea15367ee1f270378a980f4c
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-09 22:22:26 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,18 @@
+def introduction(name)
+  print "Hi, my name is #{name}.\n"
+end
+
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+
+def introduction_with_language_optional(name)
+  introduction_with_language_optional(name,"Ruby")
+end
+
+def introduction_with_language_optional(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language_optional("Bob")
